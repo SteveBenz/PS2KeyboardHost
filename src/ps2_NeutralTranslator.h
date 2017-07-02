710bdd7acfb15690e9050a078c1e8c141ba3f46b
Steve Benz <s8878992@hotmail.com>
2017-07-01 21:50:15 -0700

    Ticky-tack stuff

--- src/ps2_NeutralTranslator.h
+++ src/ps2_NeutralTranslator.h
@@ -11,13 +11,13 @@
   it here only because I hope to make this library a superset of all of the
   assorted PS2 libraries currently available.  While PS2KeyAdvanced has great
   functionality, I feel like this translation mechanism (which you can't turn off
-  in that library) is a silly thing to do.  You should either go all the way to
+  in that library) is a poor choice.  You should either go all the way to
   ASCII (and be your own keyboard controller) or have a nicely packaged collection
   of buttons.  If you're a nicely packaged collection of buttons, then you'd be
   better off switching to the PS2 ScanCode set and mapping between the keycode
   there and the function you want to enable when it's pressed.  Having a layer
   like this in between just costs you bytes and adds a new way to have things
-  go south.  So I can't recommend using this class at all, but perhaps there's a
+  go wrong.  So I can't recommend using this class at all, but perhaps there's a
   use-case I failed to imagine.
 */
 #pragma once
@@ -197,11 +197,11 @@ namespace ps2
         PS2_KEY_LANG4       = 0x99,
         PS2_KEY_LANG5       = 0xA0,
 	};
-	KeyCode operator |(KeyCode code, KeyCode modifiers) { return (KeyCode)((uint16_t)code | (uint16_t)modifiers); }
-	KeyCode operator &(KeyCode code, KeyCode modifiers) { return (KeyCode)((uint16_t)code & (uint16_t)modifiers); }
-	KeyCode &operator |=(KeyCode &code, KeyCode modifiers) { return code = (KeyCode)((uint16_t)code | (uint16_t)modifiers); }
-	KeyCode &operator &=(KeyCode &code, KeyCode modifiers) { return code = (KeyCode)((uint16_t)code & (uint16_t)modifiers); }
-	KeyCode operator ~(KeyCode code) { return (KeyCode)(~(uint16_t)code); }
+	inline KeyCode operator |(KeyCode code, KeyCode modifiers) { return (KeyCode)((uint16_t)code | (uint16_t)modifiers); }
+    inline KeyCode operator &(KeyCode code, KeyCode modifiers) { return (KeyCode)((uint16_t)code & (uint16_t)modifiers); }
+    inline KeyCode &operator |=(KeyCode &code, KeyCode modifiers) { return code = (KeyCode)((uint16_t)code | (uint16_t)modifiers); }
+    inline KeyCode &operator &=(KeyCode &code, KeyCode modifiers) { return code = (KeyCode)((uint16_t)code & (uint16_t)modifiers); }
+    inline KeyCode operator ~(KeyCode code) { return (KeyCode)(~(uint16_t)code); }
 
 	class NeutralTranslator {
 		bool isUnmake : 1;
@@ -420,7 +420,7 @@ namespace ps2
 				case KeyboardOutput::sc2_lang3: return KeyCode::PS2_KEY_LANG3;
 				case KeyboardOutput::sc2_lang4: return KeyCode::PS2_KEY_LANG4;
 				// case KeyboardOutput::sc2_LANG5: return KeyCode::PS2_KEY_LANG5;
-				default: return (KeyCode)0;
+				default: return KeyCode::PS2_NONE;
 			};
 		}
 		
@@ -467,7 +467,7 @@ namespace ps2
 				case KeyboardOutput::sc2ex_power: return KeyCode::PS2_KEY_POWER;
 				case KeyboardOutput::sc2ex_sleep: return KeyCode::PS2_KEY_SLEEP;
 				case KeyboardOutput::sc2ex_wake: return KeyCode::PS2_KEY_WAKE;
-				default: return (KeyCode)0;
+                default: return KeyCode::PS2_NONE;
 			}
 		}
 	};
